37b116222458a669c6e1949c66879ab0a83ee6ab
Willian Volpato <willian.volpato@compasso.com.br>
2019-11-05 22:38:00 -0300

    Create book listing

---
+++ bookStore/src/main/java/com/bookStore/beans/adminListBooksBean.java
@@ -0,0 +1,25 @@
+package com.bookStore.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.inject.Model;
+import javax.inject.Inject;
+
+import com.bookStore.DAO.BookDAO;
+import com.bookStore.models.Book;
+
+@Model
+public class adminListBooksBean {
+
+	@Inject
+	private BookDAO bookDAO;
+	private List<Book> books = new ArrayList<>();
+
+	public List<Book> getBooks() {
+		this.books = bookDAO.list();
+		return books;
+	}
+	
+
+}
